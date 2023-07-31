cc55113dafaa8f1268e1d2d47b1bf6e5e72274ac
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-07-31 18:44:16 -0500

    file scoped namespaces

--- Hexagone/Color.cs
+++ Hexagone/Color.cs
@@ -1,40 +1,39 @@
 using Godot;
 using System;
 
-namespace Hexagone
+namespace Hexagone;
+
+public static class Color
 {
-    public static class Color
+
+    public static readonly Godot.Color Red = new Godot.Color("#dc322f");
+    public static readonly Godot.Color Magenta = new Godot.Color("#d33682");
+    public static readonly Godot.Color Yellow = new Godot.Color("#b58900");
+    public static readonly Godot.Color Green = new Godot.Color("#859900");
+    public static readonly Godot.Color Blue = new Godot.Color("#6c71c4");
+    public static readonly Godot.Color Purple = new Godot.Color("#6c71c4");
+
+    public static Godot.Color GetColor(TileColor tileColor)
     {
-
-        public static readonly Godot.Color Red = new Godot.Color("#dc322f");
-        public static readonly Godot.Color Magenta = new Godot.Color("#d33682");
-        public static readonly Godot.Color Yellow = new Godot.Color("#b58900");
-        public static readonly Godot.Color Green = new Godot.Color("#859900");
-        public static readonly Godot.Color Blue = new Godot.Color("#6c71c4");
-        public static readonly Godot.Color Purple = new Godot.Color("#6c71c4");
-
-        public static Godot.Color GetColor(TileColor tileColor)
+        switch (tileColor)
         {
-            switch (tileColor)
-            {
-                case TileColor.Red:
-                    return Red;
-                case TileColor.Magenta:
-                    return Magenta;
-                case TileColor.Yellow:
-                    return Yellow;
-                case TileColor.Green:
-                    return Green;
-                case TileColor.Blue:
-                    return Blue;
-                case TileColor.Purple:
-                    return Purple;
+            case TileColor.Red:
+                return Red;
+            case TileColor.Magenta:
+                return Magenta;
+            case TileColor.Yellow:
+                return Yellow;
+            case TileColor.Green:
+                return Green;
+            case TileColor.Blue:
+                return Blue;
+            case TileColor.Purple:
+                return Purple;
 
-                default:
-                    //TODO better error message
-                    throw new ArgumentException("Invalid TileColor", "tileColor");
-            }
+            default:
+                //TODO better error message
+                throw new ArgumentException("Invalid TileColor", "tileColor");
         }
-
     }
+
 }
--- Hexagone/Enum.cs
+++ Hexagone/Enum.cs
@@ -1,39 +1,38 @@
-namespace Hexagone
+namespace Hexagone;
+
+public enum TileType
 {
-    public enum TileType
-    {
-        Normal,
-        Flower,
-        Pearl
-    }
+    Normal,
+    Flower,
+    Pearl
+}
 
-    public enum TileColor
-    {
-        Red,
-        Magenta,
-        Yellow,
-        Green,
-        Blue,
-        Purple,
+public enum TileColor
+{
+    Red,
+    Magenta,
+    Yellow,
+    Green,
+    Blue,
+    Purple,
 
-        Flower,
-        Pearl
-    }
+    Flower,
+    Pearl
+}
 
-    public enum Dir
-    {
-        Unknown = -1,
-        UpLeft = 0,
-        Up,
-        UpRight,
-        DownRight,
-        Down,
-        DownLeft
-    }
+public enum Dir
+{
+    Unknown = -1,
+    UpLeft = 0,
+    Up,
+    UpRight,
+    DownRight,
+    Down,
+    DownLeft
+}
 
-    public enum SpinDir
-    {
-        Clockwise,
-        AntiClockwise
-    }
+public enum SpinDir
+{
+    Clockwise,
+    AntiClockwise
 }
